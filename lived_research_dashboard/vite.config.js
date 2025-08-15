1ca77c69c59057f5d7206baf8c7965aad0a47fa5
SithuminiJayasinghe <sithu6665.@gmail.com>
2025-08-15 13:40:52 +0530

    Migrate from CRA to Vite

---
+++ lived_research_dashboard/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react({
+    include: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"] // treat .js as JSX too
+  })],
+  server: {
+    port: 3000,
+    strictPort: true
+  }
+})
