1fe796b61e7c77e366f2afba908a6f42833ae9ea
SithuminiJayasinghe <sithu6665.@gmail.com>
2025-08-16 13:07:30 +0530

    sidebar collapsible feature

--- lived_research_dashboard/src/icons/LogoutIcon.jsx
+++ lived_research_dashboard/src/icons/LogoutIcon.jsx
@@ -10,9 +10,9 @@ export default function LogoutIcon({ className = '' }) {
       className={className}
     >
       {/* Lock body */}
-      <rect x="6" y="9" width="12" height="10" rx="2" />
+      <rect x="0" y="9" width="12" height="10" rx="2" />
       {/* Open shackle */}
-      <path d="M9 9V6a3 3 0 0 1 6 0" />
+      <path d="M4 9V6a3 3 0 0 1 6 0" />
     </svg>
   );
 }
--- lived_research_dashboard/src/icons/RegionIcon.jsx
+++ lived_research_dashboard/src/icons/RegionIcon.jsx
@@ -5,16 +5,16 @@ export default function RegionIcon({ className = '' }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 24 20"
       fill="currentColor"
       className={className}
     >
       {/* Left bar */}
-      <rect x="5" y="9" width="3" height="8" rx="1.5" />
+      <rect x="0" y="9" width="3" height="8" rx="1.5" />
       {/* Middle (tallest) bar */}
-      <rect x="10.5" y="5" width="3" height="12" rx="1.5" />
+      <rect x="5.5" y="5" width="3" height="12" rx="1.5" />
       {/* Right bar */}
-      <rect x="16" y="11" width="3" height="6" rx="1.5" />
+      <rect x="11" y="11" width="3" height="6" rx="1.5" />
     </svg>
   );
 }
